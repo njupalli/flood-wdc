a6330fea7d63a1123bb26ca913c4bc108545e714
njupalli <nehajupalli06@gmail.com>
2026-04-22 18:11:31 -0400

    Add files via upload

---
+++ fetch_data.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+import time
+
+API_URL = "http://44.214.136.171:8000/predict_grid"
+
+while True:
+    try:
+        response = requests.get(API_URL)
+        data = response.json()
+
+        df = pd.DataFrame(data)
+
+        df.to_csv("flood_data.csv", index=False)
+        print("Updated data!")
+
+    except Exception as e:
+        print("Error:", e)
+
+    time.sleep(30)  # refresh every 30 seconds
